ac12c3d4a3136cfdc304a3d48334c6934e3d32c2
DavidLC578 <davidlatt2002@gmail.com>
2024-09-09 20:56:39 +0200

    pagina home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import { LogoutBTN } from "../components/LogoutBTN";
+import { Publicaciones } from "../components/Publicaciones";
+import { Plus } from "../assets/plus";
 export function Home() {
   return (
     <>
-      <h1>Home page after login</h1>
-      <LogoutBTN />
+      <div>
+        <div className="flex justify-between px-3 pt-4 pb-3 border border-black border-b-1">
+          <div className="w-8 h-8 rounded-full bg-white"></div>
+          <div>X</div>
+          <div>C</div>
+        </div>
+        {/* <Publicaciones / */}
+      </div>
+      <div className="absolute bottom-3 right-4">
+        <Plus />
+      </div>
+      {/* <LogoutBTN /> */}
     </>
   );
 }
